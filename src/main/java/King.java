8fa54f411039b310aee57dc40215e92b83cebbca
Frank Pomiano <frank.pomiano@unmsm.edu.pe>
2022-08-25 00:57:56 -0500

    refactorizacion de code smell minor 4

--- src/main/java/King.java
+++ src/main/java/King.java
@@ -45,7 +45,7 @@ public class King
         ArrayList<String> southMoves = calculateSouthMoves( board, 1 );
         ArrayList<String> eastMoves = calculateEastMoves( board, 1 );
         ArrayList<String> westMoves = calculateWestMoves( board, 1 );
-        ArrayList<String> allMoves = new ArrayList<String>();
+        ArrayList<String> allMoves = new ArrayList<>();
         allMoves.addAll( northEastMoves );
         allMoves.addAll( northWestMoves );
         allMoves.addAll( southWestMoves );
@@ -64,7 +64,7 @@ public class King
      * @return true if checked, false if not checked
      */
     public boolean isChecked( ChessGameBoard board ){
-        return getCurrentAttackers( board ).size() > 0;
+        return !getCurrentAttackers( board ).isEmpty();
     }
     /**
      * Creates an icon for this piece depending on the piece's color.
